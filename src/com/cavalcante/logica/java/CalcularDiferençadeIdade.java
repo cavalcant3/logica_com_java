f811aa7bb6a834e66a4cd8f85dd7669927f0c6ee
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-04 16:29:33 -0300

    calcular diferença de idade

---
+++ src/com/cavalcante/logica/java/CalcularDiferençadeIdade.java
@@ -0,0 +1,27 @@
+package com.cavalcante.logica.java;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class CalcularDiferençadeIdade {
+
+public static void main (String args[]){
+//    int idadeUm=13;
+//    int idadeDois=20;
+//    float resultado;
+//
+//    resultado = idadeDois - idadeUm;
+//
+//    System.out.println(resultado);
+
+    //dificultando o desafio
+//    Scanner sc = new Scanner(System.in);
+//    int idadeUm;
+//    int idadeDois;
+//    float resultado;
+//    System.out.println("Digite a primeira idade");
+//    int  idadeUm = Integer.parseInt();
+
+}
+
+}
