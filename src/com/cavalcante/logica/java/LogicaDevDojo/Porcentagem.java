2e38df44411fc8b1d5d1ec487c2838e93ec6e897
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-06 16:57:46 -0300

    ajustes

--- src/com/cavalcante/logica/java/LogicaDevDojo/Porcentagem.java
+++ src/com/cavalcante/logica/java/LogicaDevDojo/Porcentagem.java
@@ -11,12 +11,23 @@ public class Porcentagem {
             double sal;
             double porcentagem = 0.3;
             double resultado;
+            double  porcentagem2 = 0.15;
+            double  porcentagem3 = 0.5;
+
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Informe o salário:");
         sal = entrada.nextDouble();
-
+                //porcentagem 30%
         resultado = sal*porcentagem;
+
+        System.out.println(resultado);
+                //porcentagem 15%
+        resultado = sal*porcentagem2;
+
+        System.out.println(resultado);
+                //porcentagem 5%
+        resultado = sal*porcentagem3;
 
         System.out.println(resultado);
 
