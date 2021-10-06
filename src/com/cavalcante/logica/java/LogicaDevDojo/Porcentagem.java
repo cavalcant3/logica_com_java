d93776a43311af4cbfe177a4e7e070e0fa3c4c61
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-06 16:34:04 -0300

    Correção de resolução

--- src/com/cavalcante/logica/java/LogicaDevDojo/Porcentagem.java
+++ src/com/cavalcante/logica/java/LogicaDevDojo/Porcentagem.java
@@ -9,14 +9,14 @@ public class Porcentagem {
     public static void main (String args[]){
 
             double sal;
-            double porcentagem = 2;
+            double porcentagem = 0.3;
             double resultado;
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Informe o salário:");
         sal = entrada.nextDouble();
 
-        resultado = sal/porcentagem;
+        resultado = sal*porcentagem;
 
         System.out.println(resultado);
 
