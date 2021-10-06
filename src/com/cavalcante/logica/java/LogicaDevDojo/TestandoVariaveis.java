77171e1394aa9e7aa1cf9fe3ad254573dce114df
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-06 17:03:57 -0300

    testando tipos de variaveis

---
+++ src/com/cavalcante/logica/java/LogicaDevDojo/TestandoVariaveis.java
@@ -0,0 +1,14 @@
+package com.cavalcante.logica.java.LogicaDevDojo;
+
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        //psvm
+        char masculino = 'A';
+        char fmeinino = 'A';
+        String masculino2 = "olá";
+        String feminno2 = "olá";
+        boolean condicao = false;
+
+        System.out.println(condicao);
+    }
+}
