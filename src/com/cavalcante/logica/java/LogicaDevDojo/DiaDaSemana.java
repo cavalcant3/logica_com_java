769bcdde867cad87b3e47075615a2e61644ad660
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-08 09:20:09 -0300

    refatorando código

--- src/com/cavalcante/logica/java/LogicaDevDojo/DiaDaSemana.java
+++ src/com/cavalcante/logica/java/LogicaDevDojo/DiaDaSemana.java
@@ -1,7 +1,32 @@
 package com.cavalcante.logica.java.LogicaDevDojo;
 
+import java.util.Scanner;
+
 public class DiaDaSemana {
     public static void main(String[] args) {
-        
+        int DiaDaSemana;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o número");
+        DiaDaSemana = entrada.nextInt();
+
+        if (DiaDaSemana == 1) {
+            System.out.println("Hoje é domingo!");
+        } else if (DiaDaSemana == 2){
+            System.out.println("Hoje é segunda");
+        }else if (DiaDaSemana == 3){
+            System.out.println("Hoje é terça");
+        }else if (DiaDaSemana == 4){
+            System.out.println("Hoje é quarta");
+        }else if (DiaDaSemana == 5){
+            System.out.println("Hoje é quinta");
+        }else if (DiaDaSemana == 6){
+            System.out.println("Hoje é sexta");
+        }else if (DiaDaSemana == 7){
+            System.out.println("Hoje é sábado");
+        } else{
+            System.out.println("Numero invalido");
+        }
     }
 }
---
+++ src/com/cavalcante/logica/java/LogicaDevDojo/SwitchCase/DiasDaSemanaSwitch.java
@@ -0,0 +1,24 @@
+package com.cavalcante.logica.java.LogicaDevDojo.SwitchCase;
+
+public class DiasDaSemanaSwitch {
+    public static void main(String[] args) {
+        int dia = 7;
+
+        switch(dia){
+            case 1:
+                System.out.println("Hoje é domingo!");
+            case 2:
+                System.out.println("Hoje é segunda");
+            case 3:
+                System.out.println("Hoje é terça");
+            case 4:
+                System.out.println("Hoje é quarta");
+            case 5:
+                System.out.println("Hoje é quinta");
+            case 6:
+                System.out.println("Hoje é sexta");
+            case 7:
+                System.out.println("Hoje é sábado");
+        }
+    }
+}
