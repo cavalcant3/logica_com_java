3b05e368f5a01123700cd6a08d2df81997b661f4
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-10 17:17:52 -0300

    REsolução

---
+++ src/com/cavalcante/logica/java/LogicaDevDojo/Arrays/Array04.java
@@ -0,0 +1,49 @@
+package com.cavalcante.logica.java.LogicaDevDojo.Arrays;
+
+import java.util.Scanner;
+
+public class Array04 {
+    public static void main(String[] args) {
+//        int[] array1 = new int[3];
+//        int[] array2 = new int[3];
+//        int[] array3 = new int[3];
+//        Scanner entrada = new Scanner(System.in);
+//        for (int i = 0; i < array1.length ; i++) {
+//            System.out.println("Vetor 1, posição " + i + ": ");
+//            array1[i] = entrada.nextInt();
+//
+//        }
+//        System.out.println("----------------------------------------");
+//        for (int i = 0; i < array2.length ; i++) {
+//            System.out.println("Vetor 2, posição " + i + ": ");
+//            array2[i] = entrada.nextInt();
+//
+//        }
+//        System.out.println("----------------------------------------");
+//        for (int i = 0; i < array3.length ; i++) {
+//            System.out.println("Vetor 3, posição " + i + ": ");
+//            array3[i] = array1[i] * array2[i];
+//            System.out.println(array3[i]+ " de resultado");
+//
+//        }
+        int[] array1 = new int[3];
+        int[] array2 = new int[3];
+        int[] array3 = new int[3];
+        Scanner entrada = new Scanner(System.in);
+        String resultado = "  ";
+        for (int i = 0; i < array1.length ; i++) {
+            System.out.println("Vetor 1, posição " + i + ": ");
+            array1[i] = entrada.nextInt();
+            System.out.println("Vetor 2, posição " + i + ": ");
+            array2[i] = entrada.nextInt();
+            array3[i] = array1[i] * array2[i];
+            resultado = resultado + array3[i];
+
+        }
+
+
+
+   }
+
+
+}
---
+++ src/com/cavalcante/logica/java/LogicaDevDojo/lista/Q4.java
@@ -0,0 +1,6 @@
+package com.cavalcante.logica.java.LogicaDevDojo.lista;
+
+public class Q4 {
+
+    //array04
+}
