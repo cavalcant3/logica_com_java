6d690eee0d7ff5768b91846c850a2e05ebc0b0b6
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-10 17:38:09 -0300

    Arrays multidimensionais/ explicação

--- src/com/cavalcante/logica/java/LogicaDevDojo/Arrays/Bidimensional/ArrayB01.java
+++ src/com/cavalcante/logica/java/LogicaDevDojo/Arrays/Bidimensional/ArrayB01.java
@@ -1,8 +1,16 @@
 package com.cavalcante.logica.java.LogicaDevDojo.Arrays.Bidimensional;
-
+//esses são arrays multidimensionais
 public class ArrayB01 {
     public static void main(String[] args) {
         int [] [] arrayMulti = new int [2] [2];
-        System.out.println(arrayMulti[0]);
+        arrayMulti[0] [0] = 22;
+        arrayMulti[0] [1] = 11;
+        arrayMulti[1] [0] = 44;
+        arrayMulti[1] [1] = 55;
+        System.out.println(arrayMulti [0] [0]);
+        System.out.println(arrayMulti [0] [1]);
+        System.out.println(arrayMulti [1] [0]);
+        System.out.println(arrayMulti [1] [1]);
+
     }
 }
