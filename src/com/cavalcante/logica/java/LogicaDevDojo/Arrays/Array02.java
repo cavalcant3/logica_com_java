d2196a5b1299974f0ae36c2b3dea7b2e06a19267
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-10 17:00:18 -0300

    Reorganizando código

--- src/com/cavalcante/logica/java/LogicaDevDojo/Arrays/Array02.java
+++ src/com/cavalcante/logica/java/LogicaDevDojo/Arrays/Array02.java
@@ -6,20 +6,21 @@ public class Array02 {
     public static void main(String[] args) {
        double[] notas = new double[4];
        Scanner entrada = new Scanner(System.in);
-
+        double media = 0;
         for (int i = 0; i < notas.length ; i++) {
             System.out.println("Digite a nota:" + (i+1));
           notas[i] = entrada.nextDouble();
-
-
-        }
-        double media = 0;
-        for (int i = 0; i < notas.length; i++) {
             media = media + notas[i];
-            System.out.println("Essa são as notas:" + notas[i]);
             media = media/notas.length;
-
         }
         System.out.println("Essa é a media:" + media);
+
+//        for (int i = 0; i < notas.length; i++) {
+//
+//            System.out.println("Essa são as notas:" + notas[i]);
+//
+//
+//        }
+
     }
 }
