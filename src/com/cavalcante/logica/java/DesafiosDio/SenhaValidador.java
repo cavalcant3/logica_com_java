7aabb0e68dc4727994e9603d3d9491ab19ae0535
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-11 12:49:08 -0300

    ajuste

--- src/com/cavalcante/logica/java/DesafiosDio/SenhaValidador.java
+++ src/com/cavalcante/logica/java/DesafiosDio/SenhaValidador.java
@@ -1,7 +1,13 @@
 package com.cavalcante.logica.java.DesafiosDio;
 import java.io.IOException;
 import java.util.Scanner;
+/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
 
+Entrada
+A entrada é composta por vários casos de testes contendo valores inteiros.
+
+Saída
+Para cada valor lido mostre a mensagem correspondente à descrição do problema. */
 public class SenhaValidador {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
