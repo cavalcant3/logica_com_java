916450f51e8bacba2cd5033c8c0d63ffa738b3ed
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-11 12:48:44 -0300

    Resoluções

---
+++ src/com/cavalcante/logica/java/DesafiosDio/SenhaValidador.java
@@ -0,0 +1,17 @@
+package com.cavalcante.logica.java.DesafiosDio;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SenhaValidador {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+
+        int senha = leitor.nextInt();
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = leitor.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+
+    }
+}
